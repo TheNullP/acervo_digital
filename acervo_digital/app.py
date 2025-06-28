41873ee7ace4e3b513272b4741b581fc9fbe692e
TheNullP <mfreitasc0@gmail.com>
2025-06-28 18:11:59 -0300

    Primeiro commit

---
+++ acervo_digital/app.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+
+app = FastAPI()
+
+
+@app.get('/')
+def get_root():
+    return JSONResponse(content={'msg':'Hello World.'})
