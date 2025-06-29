7f223f08ea26b77132f1ebf2ff5e15bb8c71026f
TheNullP <mfreitasc0@gmail.com>
2025-06-29 10:22:23 -0300

    feat(database): Configura e inicializa ORM com tabela de usuários
    Este commit configura o ambiente do banco de dados usando SQLAlchemy
    ORM.
    - **Adiciona a tabela `users`**: Define o esquema da tabela para
    armazenamento de dados de usuários.
    - **Configura a conexão**: Estabelece a conexão com o PostgreSQL usando
    Pydantic Settings para gerenciar a URL do DB de forma segura e flexível
    entre ambientes.
    - **Inicializa sessões de banco de dados**: Prepara a infraestrutura
    para interações futuras com o banco de dados.
    
    Isso é a base para a gestão de usuários e autenticação na aplicação.

---
+++ src/acervo_digital/core/database.py
@@ -0,0 +1,26 @@
+from sqlalchemy.orm import Mapped, mapped_column, registry, sessionmaker
+from sqlalchemy import create_engine
+
+
+reg = registry()
+
+engine = create_engine('postgresql+psycopg://docker:docker@0.0.0.0:5435/docker')
+
+@reg.mapped_as_dataclass
+class User:
+    __tablename__ = 'users'
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
+
+
+reg.metadata.create_all()
+Session =  sessionmaker(engine)
+
+
+def get_db():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
