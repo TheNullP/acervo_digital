9d0f8cb18342f5a361cd85f63bad0c7eec29202c
TheNullP <mfreitasc0@gmail.com>
2025-06-29 18:26:41 -0300

    feat(user): Implementa API CRUD para gestão de usuários
    criado endpoints:
     - read: Retorna uma lista de todos os usuários registrados.
     - create: Permite o registro de novos usuários.
     - update: Habilita atualização de usário existente.

--- acervo_digital/app.py
+++ acervo_digital/app.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from acervo_digital.routers import user
 
 
 app = FastAPI()
 
+app.include_router(user.router)
 
 @app.get('/')
 def get_root():
     return JSONResponse(content={'msg':'Hello World.'})
+
---
+++ acervo_digital/routers/user.py
@@ -0,0 +1,77 @@
+from typing import Optional
+from fastapi import APIRouter, Depends, Form
+from fastapi.exceptions import HTTPException
+from fastapi.responses import JSONResponse
+from sqlalchemy.orm import Session
+from acervo_digital.schemas.user_schema import UserSchema
+
+from acervo_digital.core.database import User, get_db
+
+
+router = APIRouter(tags=["user"])
+
+
+@router.get('/readUsers')
+def read_users(
+    db: Session = Depends(get_db)
+):
+    users = db.query(User).all()
+
+    return users
+
+@router.post('/createUser')
+def create_user(
+    username: str,
+    password: str,
+    db: Session = Depends(get_db)
+):
+    exists = db.query(User).filter_by(username=username).first()
+
+    if exists:
+        raise HTTPException(
+            detail={'msg': 'Usuário ou Email já existente.'},
+            status_code=400
+        )
+    new_user = User(
+        username=username,
+        password=password
+    ) 
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return JSONResponse(
+        content={'msg':'Usuário criado com sucesso.'},
+        status_code=201
+    )
+
+
+@router.put('/updateUser')
+def update_user(
+    userId: int,
+    username: Optional[str] = Form(None),
+    password: Optional[str] = Form(None),
+    db: Session = Depends(get_db)
+):
+    exists_user = db.query(User).filter_by(id=userId).first()
+
+    if not exists_user:
+        return JSONResponse(
+            content={'msg':'Usuário não localizado.'},
+            status_code=404
+        )
+    if username:
+        exists_user.username = username
+    if password:
+        exists_user.password = password
+
+    db.add(exists_user)
+    db.commit()
+    db.refresh(exists_user)
+
+    return JSONResponse(
+        content={ 'msg': 'Atualizado' },
+        status_code=200
+    )
+
